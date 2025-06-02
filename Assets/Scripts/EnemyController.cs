d07f7eb92493170d2a26fc725f35e987ed7c5a67
Jacques <173047302+Jacques6776@users.noreply.github.com>
2025-06-02 07:49:36 +0200

    Commit 3
    Added enemy that can patrol and follow the player

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,10 +4,59 @@ public class EnemyController : MonoBehaviour
 {
     //Enemy Movement controlls
     [SerializeField]
-    private Vector2 initialMoveSpeed = new Vector2(2f, 6f);
+    private float minPatrolSpeed = 3f;
     [SerializeField]
-    private float chaseSpeed = 4f;
+    private float maxPatrolSpeed = 8f;
     private float currentMoveSpeed;
 
+    //Chase controlls
+    [SerializeField]
+    private float chaseSpeed = 4f;
+    [SerializeField]
+    private float engageDistance = 5f;
 
+    private Rigidbody enemyRb;
+    private PlayerController playerObject;
+
+    private void Awake()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        playerObject = FindFirstObjectByType<PlayerController>();
+        currentMoveSpeed = Random.Range(minPatrolSpeed, maxPatrolSpeed);
+    }
+
+    private void Update()
+    {
+        if ((playerObject.transform.position - transform.position).magnitude < engageDistance)
+        {
+            Debug.Log("In range");
+            Vector3 move = (playerObject.transform.position - transform.position).normalized;
+            enemyRb.linearVelocity = move * chaseSpeed;
+            transform.LookAt(playerObject.transform);
+        }
+
+        if ((playerObject.transform.position - transform.position).magnitude > engageDistance)
+        {
+            PatrolState();
+        }
+    }
+
+    private void PatrolState()
+    {
+        enemyRb.linearVelocity = transform.forward * currentMoveSpeed;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Wall"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, engageDistance);
+    }
 }
